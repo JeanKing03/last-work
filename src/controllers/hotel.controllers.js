51f79a37b2c867dcb818f66729f48600904d1712
JeanKingG <137098569+JeanKing03@users.noreply.github.com>
2024-10-22 20:54:10 -0400

    I need make hotel test

--- src/controllers/hotel.controllers.js
+++ src/controllers/hotel.controllers.js
@@ -28,7 +28,7 @@ const update = catchError(async (req, res) => {
   const { id } = req.params;
   const result = await updateServices(req.body, id);
   if (result[0] === 0) return res.sendStatus(404);
-  return res.json(result[1]);
+  return res.json(result[1][0]);
 });
 
 const remove = catchError(async (req, res) => {
